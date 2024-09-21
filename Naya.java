c6c13ec50860f477dd29adad0a29e2e10e8dec70
Songehyun <songehyun00@gmail.com>
2024-09-21 23:17:42 +0900

    자바로 헬로 월드! 기본 문법 어떤지 확인을 해보자

---
+++ Naya.java
@@ -0,0 +1,16 @@
+public class Naya {
+  public static void main(String[] args) {
+    String name = "송이현";
+    int age = 31;
+    double height = 177.5;
+    boolean student = false;
+    char blood = 'B';
+
+
+    System.out.println("나의 이름은?" + name);
+    System.out.println("이게 내 나이야" + age);
+    System.out.println("이게 내 키야" + height);
+    System.out.println("난 학생이야?" +  student);
+    System.out.println("이게 내 혈액형이야" + blood);
+  }
+}
